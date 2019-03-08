b18d9916bf0eab140a357802b98dc48b38bef355
Fantix King <fantix@uchicago.edu>
2019-03-08 16:01:05 -0600

    Cut from gh:uc-cdis/template-repo

--- python/setup.py
+++ python/setup.py
@@ -1,15 +1,35 @@
+"""
+This is a file to describe the Python module distribution and
+helps with installation.
+
+More info on various arguments here:
+https://setuptools.readthedocs.io/en/latest/setuptools.html
+"""
 from setuptools import setup, find_packages
+from subprocess import check_output
+
+
+def get_version():
+    # https://github.com/uc-cdis/dictionaryutils/pull/37#discussion_r257898408
+    try:
+        tag = check_output(
+            ["git", "describe", "--tags", "--abbrev=0", "--match=[0-9]*"]
+        )
+        return tag.decode("utf-8").strip("\n")
+    except Exception:
+        raise RuntimeError(
+            "The version number cannot be extracted from git tag in this source "
+            "distribution; please either download the source from PyPI, or check out "
+            "from GitHub and make sure that the git CLI is available."
+        )
+
 
 setup(
-    name="rbac",
-    version="0.1.0",
-    install_requires=[
-        "backoff>=1.6.0,<2.0.0",
-        "requests>=2.18.0,<3.0.0"
-    ],
-    scripts=[],
-    dependency_links=[
-        "git+https://git@github.com/uc-cdis/cdiserrors.git@master#egg=cdiserrors",
-    ],
+    name="gen3rbac",
+    version=get_version(),
+    description="Gen3 RBAC Client",
+    url="https://github.com/uc-cdis/rbac-client",
+    license="Apache",
     packages=find_packages(),
+    install_requires=["cdiserrors~=0.1", "backoff~=1.6", "requests~=2.18"],
 )
